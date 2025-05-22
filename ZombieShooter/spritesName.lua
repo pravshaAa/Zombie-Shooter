731551aaf12a652c7b9ded8e693e3f0be0a03d5a
pravshaAa <141495151+pravshaAa@users.noreply.github.com>
2025-05-22 14:06:11 +0300

    Add files via upload

---
+++ ZombieShooter/spritesName.lua
@@ -0,0 +1,23 @@
+
+love.graphics.setDefaultFilter("linear", "nearest")
+
+PlayerDefaultDown = love.graphics.newImage("sprites/Zombie-Tileset---_0476_Capa-477.png")
+PlayerDefaultUp = love.graphics.newImage("sprites/Zombie-Tileset---_0482_Capa-483.png")
+PlayerDefaultRight = love.graphics.newImage("sprites/Zombie-Tileset---_0479_Capa-480.png")
+PlayerDefaultLeft = love.graphics.newImage("sprites/Zombie-Tileset---_0485_Capa-486.png")
+
+ZombieDefaultDown = love.graphics.newImage("sprites/Zombie-Tileset---_0394_Capa-395.png")
+ZombieDefaultUp = love.graphics.newImage("sprites/Zombie-Tileset---_0400_Capa-401.png")
+ZombieDefaultRight = love.graphics.newImage("sprites/Zombie-Tileset---_0405_Capa-406.png")
+ZombieDefaultLeft = love.graphics.newImage("sprites/Zombie-Tileset---_0399_Capa-400.png")
+
+
+PlayerGoDownTable = {a = love.graphics.newImage("sprites/Zombie-Tileset---_0477_Capa-478.png"), b = love.graphics.newImage("sprites/Zombie-Tileset---_0478_Capa-479.png")}
+PlayerGoUpTable = {a = love.graphics.newImage("sprites/Zombie-Tileset---_0483_Capa-484.png"), b = love.graphics.newImage("sprites/Zombie-Tileset---_0484_Capa-485.png")}
+PlayerGoRightTable = {a = love.graphics.newImage("sprites/Zombie-Tileset---_0480_Capa-481.png"), b = love.graphics.newImage("sprites/Zombie-Tileset---_0481_Capa-482.png")}
+PlayerGoLeftTable = {a = love.graphics.newImage("sprites/Zombie-Tileset---_0486_Capa-487.png"), b = love.graphics.newImage("sprites/Zombie-Tileset---_0487_Capa-488.png")}
+
+ZombieGoDownTable = {a = love.graphics.newImage("sprites/Zombie-Tileset---_0395_Capa-396.png"), b = love.graphics.newImage("sprites/Zombie-Tileset---_0396_Capa-397.png")}
+ZombieGoUpTable = {a = love.graphics.newImage("sprites/Zombie-Tileset---_0401_Capa-402.png"), b = love.graphics.newImage("sprites/Zombie-Tileset---_0402_Capa-403.png")}
+ZombieGoRightTable = {a = love.graphics.newImage("sprites/Zombie-Tileset---_0404_Capa-405.png"), b = love.graphics.newImage("sprites/Zombie-Tileset---_0403_Capa-404.png")}
+ZombieGoLeftTable = {a = love.graphics.newImage("sprites/Zombie-Tileset---_0398_Capa-399.png"), b = love.graphics.newImage("sprites/Zombie-Tileset---_0397_Capa-398.png")}
