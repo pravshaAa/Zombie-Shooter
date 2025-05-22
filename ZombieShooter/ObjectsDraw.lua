671a7e7fe036779e3b4ce5cf752ee35fb61337b6
pravshaAa <141495151+pravshaAa@users.noreply.github.com>
2025-05-22 14:07:28 +0300

    Update ObjectsDraw.lua

--- ZombieShooter/ObjectsDraw.lua
+++ ZombieShooter/ObjectsDraw.lua
@@ -1,24 +1,26 @@
 
 function ObjectsFunc(table)
-	local width = 16
-	local height = 16
-	return {
-		Draw = function(self)
-			for i, v in ipairs(table) do
-				if table[i].typeObjects == "pp" then
-					local width = table[i].width or 16
-					local height = table[i].height or 16
-					love.graphics.setColor(1,0,0)
-					love.graphics.rectangle("fill", table[i].x, table[i].y, width, height)
-				elseif table[i].typeObjects == "pt" then
-					local width = table[i].width or 16
-					local height = table[i].height or 16
-					love.graphics.setColor(0,0,1)
-					love.graphics.rectangle("fill", table[i].x, table[i].y, width, height)
-				end
-			end
-		end
-	}
+    return {
+        Draw = function(self)
+            for i, v in ipairs(table) do
+                self:DrawSingle(v)
+            end
+        end,
+
+        DrawSingle = function(self, obj)
+            if obj.typeObjects == "pp" then
+                local width = obj.width or 16
+                local height = obj.height or 16
+                love.graphics.setColor(1, 0, 0)
+                love.graphics.rectangle("fill", obj.x, obj.y, width, height)
+            elseif obj.typeObjects == "pt" then
+                local width = obj.width or 16
+                local height = obj.height or 16
+                love.graphics.setColor(0, 0, 1)
+                love.graphics.rectangle("fill", obj.x, obj.y, width, height)
+            end
+        end
+    }
 end
 
 return ObjectsDraw
